d31ba6a32d5a3fc41001c81eabfcf311c91f20a8
Lorenz <narvaezlorenz395@gmail.com>
2025-11-04 22:17:18 +0800

    Another changes of modal

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 const modal = document.getElementById("forgotModal");
 const modal2 = document.getElementById("TermsModal");
-const body = document.body;
 
 function openModal() {
   modal.style.display = "flex";
-  body.classList.add("blurred");
 }
 
 function closeModal() {
   modal.style.display = "none";
-  body.classList.remove("blurred");
 }
 
 function openModal2() {
   modal2.style.display = "flex";
-  body.classList.add("blurred");
 }
 
 function closeModal2() {
   modal2.style.display = "none";
-  body.classList.remove("blurred");
 }
