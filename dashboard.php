e31af3bd390c82ce29c5fb00227c70c48709d217
Lorenz <narvaezlorenz395@gmail.com>
2025-11-07 22:29:24 +0800

    User Dashboard

--- dashboard.php
+++ dashboard.php
@@ -2,57 +2,86 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Online Banking</title>
-  <link rel="stylesheet" href="dashboarddd.css" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online banking</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@latest/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="dashboard.css">
 </head>
 
 <body>
-  <div class="page">
-    <div class="nav">
-      <div class="Logo">LOGO</div>
-      <nav>
-        <ul>
-          <li>Dashboard</li>
-          <li>Transfer</li>
-          <li>Inbox</li>
-          <li>My Cards</li>
-          <li>Loan Services</li>
-        </ul>
-      </nav>
-      <div class="logout"><button>Logout</button></div>
-    </div>
-    <div class="contents">
-      <div class="greet">
-        <h2>Welcome Back</h2>
-        <h1>Lorenz</h1>
-      </div>
-      <div class="main-content">
-        <div class="balance">
-          <h2>Available balance</h2>
-          <h1>5000</h1>
+    <div class="whole">
+        <div class="nav-section">
+            <div class="Logo">LOGO</div>
+            <a href="" class="active">
+                <div class="Dashboard">
+                    <i class="bi bi-house"></i> Dashboard
+                </div>
+            </a>
+            <a href="">
+                <div class="Account">
+                    <i class="bi bi-person"></i> Account
+                </div>
+            </a>
+            <a href="">
+                <div class="Cards">
+                    <i class="bi bi-credit-card"></i> Cards
+                </div>
+            </a>
+            <a href="">
+                <div class="Loan">
+                    <i class="bi bi-cash"></i> Loan
+                </div>
+            </a>
+            <a href="">
+                <div class="Inbox">
+                    <i class="bi bi-envelope"></i> Inbox
+                </div>
+            </a>
+            <a href="">
+                <div class="Settings">
+                    <i class="bi bi-gear"></i> Settings
+                </div>
+            </a>
         </div>
-        <div class="cash-in">
-          <button>Cash In</button>
-          <button>Manage Cards</button>
+        <div class="page">
+            <div class="profile" id="profile">
+                <a href="#" onclick="openModal3(event)"><i class="bi bi-box-arrow-right" title="Logout"
+                        style="font-size: 25px; color;"></i></a>
+                <a href="#"><i class="bi bi-person-circle" title="Profile" style="font-size: 25px; color;"></i></a>
+                <a href="#"><i class="bi bi-bell-fill" title="Notification" style="font-size: 25px; color;"></i></a>
+            </div>
+            <div class="greet">
+                <h1>Welcome back</h1>
+                <div class="cards">
+                    <div class="card-design" id="card1"></div>
+                    <div class="card-design" id="card2"></div>
+                    <div class="card-design" id="card3"></div>
+                </div>
+            </div>
+            <div class="section">
+                <div class="balance">
+                    <h3>Account Balance</h3>
+                </div>
+                <div class="transaction">
+                    <h3>Transactions</h3>
+                </div>
+            </div>
         </div>
-        <div class="recent-transactions">
-          <h2>Transactions</h2>
-          <!-- Dynamic -->
+    </div>
+    <div id="logoutModal" class="modal">
+        <div class="modal-content">
+            <h2>Logout</h2>
+            <div class="scrollable">
+                <p>
+                    Logout your accout?
+                </p>
+            </div>
+            <button class="confirm-btn" onclick="confirmLogout()">Logout</button>
+            <button class="close-btn" onclick="closeModal3()">Close</button>
         </div>
-      </div>
     </div>
-    <div class="shorcut">
-      <div class="Savings">
-        <h2>Savings</h2>
-        <p>25000</p>
-      </div>
-      <div class="recent">
-        <h2>Mutuals</h2>
-      </div>
-    </div>
-  </div>
+    <script src="script.js"></script>
 </body>
 
 </html>
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const modal = document.getElementById("forgotModal");
-const modal2 = document.getElementById("TermsModal");
 
 function openModal() {
   modal.style.display = "flex";
@@ -9,10 +8,27 @@ function closeModal() {
   modal.style.display = "none";
 }
 
+const modal2 = document.getElementById("TermsModal");
+
 function openModal2() {
   modal2.style.display = "flex";
 }
 
 function closeModal2() {
   modal2.style.display = "none";
+}
+
+const modal3 = document.getElementById("logoutModal");
+
+function openModal3(event) {
+  event.preventDefault();
+  modal3.style.display = "flex";
+}
+
+function closeModal3() {
+  modal3.style.display = "none";
+}
+
+function confirmLogout() {
+  window.location.href = "index.php";
 }
